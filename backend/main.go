a6b8dcdf26c0c664aa9a8cedf46cf04ded81762a
Hammad Saleem <hammadsaleem@Hammads-MacBook-Air.local>
2025-09-24 21:14:39 +0100

    basic formula setup

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Welcome to Best Mortgage!")
+
+	var loan_amount float64 = 158000
+	var term_years float64 = 30
+	var interest_rate float64 = 4.5
+	result := calcMortgage(loan_amount, term_years, interest_rate)
+
+	fmt.Printf("Monthy Repayments are %v \n", result)
+}
+
+func calcMortgage(loan_amount float64, term_years float64, interest_rate float64) float64 {
+	n := term_years * 12 // number of monthly payments
+	r := interest_rate / 12 / 100
+	result := loan_amount * r * math.Pow(1+r, n) / (math.Pow(1+r, n) - 1)
+	result = math.Round(result*100) / 100
+	return result
 }
