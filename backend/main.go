11781c8a5fd1f670632e55b16834744d96dceda5
Hammad Saleem <hammadsaleem@Hammads-MacBook-Air.local>
2025-10-06 21:09:55 +0100

    tidying up code, first time buyer and remortgage endpoints added

--- backend/main.go
+++ backend/main.go
@@ -1,99 +1,131 @@
 package main
 
 import (
-	"best-mortgage/providers/banks"
-
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	user_mortgage_type := "first_time_buyer"
-	// user_mortgage_type := "remortgage"
-	// user_mortgage_type := "home-mover"
-
 	// centralbank.PrintCentralBankRates()
-	bankRates, err := banks.LoadRates()
+	handleRequests()
+
+}
+
+type Rates struct {
+	New         map[string]float64 `json:"new"`
+	Outstanding map[string]float64 `json:"outstanding"`
+	Green       map[string]float64 `json:"green"`
+	Variable    map[string]float64 `json:"variable"`
+}
+
+type Bank struct {
+	BankName string `json:"bank_name"`
+	Rates    Rates  `json:"rates"`
+}
+
+type BankRates struct {
+	ReportDate string `json:"reporting_date"`
+	Banks      []Bank `json:"banks"`
+}
+
+type BankResponse struct {
+	BankName string                        `json:"bank_name`
+	Rates    map[string]map[string]float64 `json:"rates`
+}
+
+type Response struct {
+	ReportDate string         `json:"reporting_date`
+	Banks      []BankResponse `json:"banks"`
+}
+
+func LoadRates() (BankRates, error) {
+	data, err := os.ReadFile("banks/data.json")
 	if err != nil {
-		fmt.Println("Error", err)
+		fmt.Println("Error reading data", err)
+		return BankRates{}, err
 	}
 
-	// type Bank struct {
-	// }
+	var rates BankRates
+	err = json.Unmarshal(data, &rates)
+	if err != nil {
+		fmt.Println("Error parsing json", err)
+		return BankRates{}, err
 
-	// type Response struct {
-	// 	ReportingDate string `json:"reporting_date"`
-	// 	Banks         []Bank `json:"banks"`
-	// }
+	}
+	return rates, nil
+}
+
+func BuildResponse(bankRates BankRates, userType string) Response {
+	resp := Response{
+		ReportDate: bankRates.ReportDate,
+		Banks:      []BankResponse{},
+	}
+
 	for _, bank := range bankRates.Banks {
-		if user_mortgage_type == "first_time_buyer" {
-			fmt.Println("You qualify for the following options")
-			fmt.Println(bank.BankName, "First Time buyer", bank.Rates.New)
-			fmt.Println(bank.BankName, "Green Mortgage", bank.Rates.Green)
-			fmt.Println(bank.BankName, "Variable Rates", bank.Rates.Variable)
+		b := BankResponse{
+			BankName: bank.BankName,
+			Rates:    map[string]map[string]float64{},
 		}
+
+		switch userType {
+		case "first_time_buyer":
+			b.Rates["new"] = bank.Rates.New
+			b.Rates["green"] = bank.Rates.Green
+			b.Rates["variable"] = bank.Rates.Variable
+		case "remortgage":
+			b.Rates["outstanding"] = bank.Rates.Outstanding
+			b.Rates["variable"] = bank.Rates.Variable
+		}
+
+		resp.Banks = append(resp.Banks, b)
 	}
-
-	// fmt.Println(bank.BankName, bank.Fixed1To3Green)
-	// }
-
-	// handleRequests()
-
-	// var loan_amount float64 = 158000
-	// var term_years float 64 = 30
-	// var interest_rate float64 = 4.5
-	// result := calcMortgage(loan_amount, term_years, interest_rate)
-
-	// fmt.Printf("Monthy Repayments are %v \n", result)
-
-	// func calcMortgage(loan_amount float64, term_years float64, interest_rate float64) float64 {
-	// 	n := term_years * 12 // number of monthly payments
-	// 	r := interest_rate / 12 / 100
-	// 	result := loan_amount * r * math.Pow(1+r, n) / (math.Pow(1+r, n) - 1)
-	// 	result = math.Round(result*100) / 100
-	// 	return result
-	// }
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to Best Mortgage!")
-	fmt.Fprint(w, "Available Endpoints")
-}
-
-func getRates(w http.ResponseWriter, r *http.Request) {
-	type Rate struct {
-		Type         string  `json:title`
-		Provider     string  `json:provider`
-		Duration     float64 `json:duration`
-		InterestRate float64 `json:interestRate`
-	}
-
-	type Rates []Rate
-
-	rates := Rates{
-		Rate{Type: "Fixed",
-			Provider:     "AIB",
-			Duration:     3,
-			InterestRate: 4.5},
-		Rate{Type: "Fixed",
-			Provider:     "PTSB",
-			Duration:     5,
-			InterestRate: 4.35},
-		Rate{Type: "Variable",
-			Provider:     "Revolut",
-			Duration:     2.5,
-			InterestRate: 4.15},
-	}
-
-	fmt.Println("Endpoint: Rates endpoint")
-	json.NewEncoder(w).Encode(rates)
+	return resp
 }
 
 func handleRequests() {
+	bankRates, err := LoadRates()
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/rates", getRates)
+	http.HandleFunc("/rates/first_time_buyer", func(w http.ResponseWriter, r *http.Request) {
+		response := BuildResponse(bankRates, "first_time_buyer")
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+	http.HandleFunc("/rates/remortgage", func(w http.ResponseWriter, r *http.Request) {
+		response := BuildResponse(bankRates, "remortgage")
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
 	// http.HandleFunc("/centralbank/latest", )
 	log.Fatal(http.ListenAndServe(":8001", nil))
+}
+
+// var loan_amount float64 = 158000
+// var term_years float 64 = 30
+// var interest_rate float64 = 4.5
+// result := calcMortgage(loan_amount, term_years, interest_rate)
+
+// fmt.Printf("Monthy Repayments are %v \n", result)
+
+// func calcMortgage(loan_amount float64, term_years float64, interest_rate float64) float64 {
+// 	n := term_years * 12 // number of monthly payments
+// 	r := interest_rate / 12 / 100
+// 	result := loan_amount * r * math.Pow(1+r, n) / (math.Pow(1+r, n) - 1)
+// 	result = math.Round(result*100) / 100
+// 	return result
+// }
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Welcome to Best Mortgage. This project is currently in development. The following endpoints provides test data only.<br>")
+	fmt.Fprint(w, `<br>Available Endpoints<br>`)
+	fmt.Fprint(w, `First Time Buyer Rates : <a href="http://localhost:8001/rates/first_time_buyer">First Time Buyer</a><br>`)
+	fmt.Fprint(w, `Remortgage Rates : <a href="http://localhost:8001/rates/remortgage">Remortgage </a><br>`)
+
 }
